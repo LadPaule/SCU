227ca67d196f52dc216b6b6c664dced6dee617a3
LadPaule <ssozipaule@gmail.com>
2021-11-17 20:50:09 +0300

    passHref

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     <header className="flex-col">
       {/* secondary Menu */}
       <div className="flex bg-scu_blue-light items-center flex-col sm:flex-grow justify-between lg:flex-row">
-          <Link href="/">
+          <Link href="/" passHref={true}>
               <Image className="object-contain p-5 ml-4" src="/sculogo.svg" alt="logo" width={180} height={70} 
                 className="cursor-pointer hover:scale-105"
               />
@@ -17,7 +17,7 @@ function Header() {
         <div className="flex justify-evenly space-x-4 last:ml-10 max-w-2xllg">
           <MenuIcon className="text-scu_yellow h-8 hover:text-white lg:hidden"/>
           <div className=" flex space-x-6">
-            <Link href="mailto:info@smilecharityuganda.org">
+            <Link href="mailto:info@smilecharityuganda.org" passHref={true}>
               <a href="mailto:info@smilecharityuganda.org" target="_blank">
               <svg className="fill-current cursor-pointer text-scu_yellow hover:text-white hover:scale-105 hover:animate-pulse" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z"/></svg>
               </a>
