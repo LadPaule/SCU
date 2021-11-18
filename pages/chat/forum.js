25a342f8c3b3d264893972b4668fd7ef4a3c85d5
LadPaule <ssozipaule@gmail.com>
2021-11-18 07:31:09 +0300

    sorted module import

--- pages/chat/forum.js
+++ pages/chat/forum.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from "../../components/Footer"
+import Header from "../../components/Header";
 
 function forum() {
   return (
