af13836ed17667fbd02826646aa9b711a702db0f
LadPaule <ssozipaule@gmail.com>
2022-01-17 12:50:32 +0300

    children lists update

--- src/pages/children_list.js
+++ src/pages/children_list.js
@@ -1,15 +1,19 @@
 import Head from "next/head";
 import { GraphQLClient } from "graphql-request";
+import { gql } from "graphql-request";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
-import ReactPaginate from "react-paginate";
-import Router, { withRouter } from "next/router";
 import Childrenfeed from "../components/Childrenfeed";
 
+const graphcms = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
+const limit = 1;
+
 function ChildrenList({ waitingLists }) {
+
+  // console.log(currentPageNumber, hasNextPage, hasPreviousPage);
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-300">
       <Head>
         <title>Sponsor a Child</title>
         <link
