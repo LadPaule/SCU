b6f12cf57121cb843bf36f969de3994bd256e96a
LadPaule <ssozipaule@gmail.com>
2022-02-09 12:41:16 +0300

    about up

--- src/components/Vols.jsx
+++ src/components/Vols.jsx
@@ -11,7 +11,7 @@ export const vols = [
     name: "Silver Brown",
     designation: "Accounts Manager",
     story:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "I want to serve in environments that I will work to preserve and ameliorate to a better status than I find it. An environment that will also make me better each day. My areas of interest are accounting, setting up systems for financial management as well as content writing and optimization. I want to be involved with a team of organized and well-focused people with a passion to serve society.",
     photo: "/team/silver.webp",
     url: "https://www.linkedin.com/in/nowgetonline",
   },
@@ -19,7 +19,7 @@ export const vols = [
     name: "Ingabire Benigne",
     designation: "Human Resource Manager",
     story:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "My ultimate Goal is to exalt Jesus as my Lord and Savior in everything I am, say and do. I desire to grow in the knowledge of God, bear fruits and to live a life worthy of the Lord and please Him in every way.  I am lovingly desire to care for others with the best I can because I believe every Human is God’s special and valued possession (1Peter 2:9)",
     photo: "/team/bennie.webp",
     url: "https://www.linkedin.com/in/ingabire-benigne-913833230",
   },
@@ -27,7 +27,7 @@ export const vols = [
     name: "Kizito Joseph",
     designation: "Field Manager",
     story:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "I am a born-again Christian who believes in the Biblical doctrines of the Trinity, the Birth of Jesus Christ the Son of the one true God, the Power of the Holy Spirit and the new life after physical death. Though enthusiastic, I believe in being a work in process by Jesus Christ, looking at the growth and improvement of my life in all aspects every day. ",
     photo: "/team/joseph.webp",
     url: "https://www.linkedin.com/in/joseph-kizito-7a6b023b/",
   },
@@ -43,7 +43,7 @@ export const vols = [
     name: "Kobusingye Daphine",
     designation: "Youth Co-ordinator",
     story:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "If there is one thing that I know, I love and enjoy doing the will of God. I hold a strong faith in the love Jesus Christ has towards me.  Truthfully, there is nothing I enjoy more than passing on the same love to the little children and young youth of the World through ministry. I firmly believe that sharing my talents and abilities as a Christian is the way to go.",
     photo: "/team/ddk.webp",
     url: "https://www.linkedin.com/in/gabiti-joseph-mukiibi-395550230/",
   },
@@ -59,7 +59,7 @@ export const vols = [
     name: "Kakooza Francis",
     designation: "Security Observer",
     story:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "I am a Catholic. I acknowledge that Jesus is the Lord of. My personal conduct is being humble and respecting everyone. My character extends peace, joy and Love that are fundamental qualities for my personal progress and opportunity to challenge my boundaries, work on my weakness and use my strengths to elevate the work of Smile Charity Uganda team.",
     photo: "/team/kakooza.webp",
     url: "https://www.linkedin.com/in/francis-kakooza-3b26a7230",
   },
@@ -67,7 +67,7 @@ export const vols = [
     name: "Ssozi Paul",
     designation: "ICT & Media Head",
     story:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "I am self-willed and enthusiastic individual with a deep desire to know and learn every single day. When I am not designing and learning about current and upcoming tech trends, I like to do more tech.  I desire to grow closer to God knowing that Christ is Lord. I ascribe dignity, respect and love in all that I do allowing room for reproof, correction and training. ",
     photo: "/team/me.webp",
     url: "https://www.linkedin.com/in/paul-ssozi-7b4b2116a",
   }, 
